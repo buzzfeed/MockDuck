6e7f9d2e50ad060451e0e604c8de95c7a27143ab
Sebastian Celis <sebastian@sebastiancelis.com>
2018-09-06 10:41:13 -0500

    Add initial unit tests

--- MockDuckTests/Sources/RequestHandlerTests.swift
+++ MockDuckTests/Sources/RequestHandlerTests.swift
@@ -1,35 +1,154 @@
 //
-//  MockDuckTests.swift
+//  RequestHandlerTests.swift
 //  MockDuckTests
 //
 //  Created by Peter Walters on 3/19/18.
 //  Copyright © 2018 BuzzFeed, Inc. All rights reserved.
 //
 
-import XCTest
 @testable import MockDuck
+import XCTest
 
-class MockDuckTests: XCTestCase {
+class RequestHandlerTests: XCTestCase {
+
+    let testURL: URL! = URL(string: "https://www.buzzfeed.com/some-invalid-url")
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        MockDuck.shouldFallbackToNetwork = false
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        MockDuck.unregisterAllRequestHandlers()
+        MockDuck.shouldFallbackToNetwork = true
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRequestHandlerStatusCode() {
+        let statusCode = 201
+
+        MockDuck.registerRequestHandler { request in
+            return try! request.mockResponse(statusCode: statusCode)
+        }
+
+        let expectation = self.expectation(description: "Mocked network request")
+        let task = URLSession.shared.dataTask(with: testURL) { data, response, error in
+            let response = response as! HTTPURLResponse
+            XCTAssertEqual(response.statusCode, statusCode)
+            expectation.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testRequestHandlerHeader() {
+        let headerName = "X-BUZZFEED-TEST"
+        let headerValue = "Some Value"
+
+        MockDuck.registerRequestHandler { request in
+            return try! request.mockResponse(headers: [headerName : headerValue])
         }
+
+        let expectation = self.expectation(description: "Mocked network request")
+        let task = URLSession.shared.dataTask(with: testURL) { data, response, error in
+            let response = response as! HTTPURLResponse
+            XCTAssertEqual(response.headers?[headerName], headerValue)
+            expectation.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
+    }
+
+    func testRequestHandlerData() {
+        let mockData = Data(bytes: [1, 5, 2, 4])
+
+        MockDuck.registerRequestHandler { request in
+            return try! request.mockResponse(data: mockData)
+        }
+
+        let expectation = self.expectation(description: "Mocked network request")
+        let task = URLSession.shared.dataTask(with: testURL) { data, response, error in
+            XCTAssertEqual(data, mockData)
+            expectation.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
+    }
+
+    func testRequestHandlerJSON() {
+        let json: [String: Any] = [
+            "a": "value",
+            "b": 5,
+            "c": true
+        ]
+
+        MockDuck.registerRequestHandler { request in
+            return try! request.mockResponse(json: json)
+        }
+
+        let expectation = self.expectation(description: "Mocked network request")
+        let task = URLSession.shared.dataTask(with: testURL) { data, response, error in
+            let responseJSON = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            XCTAssertEqual(responseJSON["a"] as! String, "value")
+            XCTAssertEqual(responseJSON["b"] as! Int, 5)
+            XCTAssertEqual(responseJSON["c"] as! Bool, true)
+            expectation.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
+    }
+
+    func testRequestHandlerMatching() {
+        let statusCode1 = 218
+        let statusCode2 = 420
+        let url1: URL! = URL(string: "https://www.buzzfeed.com/seven-greatest-things-about-salad")
+        let url2: URL! = URL(string: "https://tasty.co/pineapple-pizza-is-amazing")
+        let expectation1 = self.expectation(description: "Network request one")
+        let expectation2 = self.expectation(description: "Network request two")
+
+        MockDuck.registerRequestHandler { request in
+            if request.url?.host == "www.buzzfeed.com" {
+                return try! request.mockResponse(statusCode: statusCode1)
+            } else if request.url?.host == "tasty.co" {
+                return try! request.mockResponse(statusCode: statusCode2)
+            } else {
+                return nil
+            }
+        }
+
+        let task1 = URLSession.shared.dataTask(with: url1) { data, response, error in
+            let response = response as! HTTPURLResponse
+            XCTAssertEqual(response.statusCode, statusCode1)
+            expectation1.fulfill()
+        }
+        task1.resume()
+
+        let task2 = URLSession.shared.dataTask(with: url2) { data, response, error in
+            let response = response as! HTTPURLResponse
+            XCTAssertEqual(response.statusCode, statusCode2)
+            expectation2.fulfill()
+        }
+        task2.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
+    }
+
+    func testNotFallingBackToNetwork() {
+        MockDuck.shouldFallbackToNetwork = false
+
+        let expectation = self.expectation(description: "Mocked network request")
+        let task = URLSession.shared.dataTask(with: testURL) { data, response, error in
+            let error = error! as NSError
+            XCTAssertEqual(error.domain, NSURLErrorDomain)
+            XCTAssertEqual(error.code, NSURLErrorNotConnectedToInternet)
+            expectation.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: 2.0, handler: nil)
     }
 }
---
+++ MockDuckTests/Sources/RequestHashTests.swift
@@ -0,0 +1,81 @@
+//
+//  RequestHashTests.swift
+//  MockDuckTests
+//
+//  Created by Sebastian Celis on 9/5/18.
+//  Copyright © 2018 BuzzFeed, Inc. All rights reserved.
+//
+
+@testable import MockDuck
+import XCTest
+
+class RequestHashTests: XCTestCase {
+    override func tearDown() {
+        MockDuck.delegate = nil
+        super.tearDown()
+    }
+
+    func testSameRequestSameHash() {
+        let url1: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=bar")
+        let url2: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=bar")
+        let request1 = URLRequest(url: url1)
+        let request2 = URLRequest(url: url2)
+        XCTAssertEqual(request1.requestHash, request2.requestHash)
+    }
+
+    func testDifferentQueryDifferentHash() {
+        let url1: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=bar")
+        let url2: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=baz")
+        let request1 = URLRequest(url: url1)
+        let request2 = URLRequest(url: url2)
+        XCTAssertNotEqual(request1.requestHash, request2.requestHash)
+    }
+
+    func testDifferentFragmentDifferentHash() {
+        let url1: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests#utm_term=ugh")
+        let url2: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests#utm_term=ouch")
+        let request1 = URLRequest(url: url1)
+        let request2 = URLRequest(url: url2)
+        XCTAssertNotEqual(request1.requestHash, request2.requestHash)
+    }
+
+    func testDifferentBodyDifferentHash() {
+        let url: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests")
+        var request1 = URLRequest(url: url)
+        var request2 = URLRequest(url: url)
+        request1.httpBody = Data(bytes: [1, 2, 3, 4])
+        request2.httpBody = Data(bytes: [1, 2, 3, 6])
+        XCTAssertNotEqual(request1.requestHash, request2.requestHash)
+    }
+
+    func testNormalizedRequestsSameHash() {
+        class TestMockDuckDelegate: MockDuckDelegate {
+            func normalizedRequest(for request: URLRequest) -> URLRequest {
+                var request = request
+                request.httpBody = nil
+
+                if
+                    let url = request.url,
+                    var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+                {
+                    components.fragment = nil
+                    components.queryItems = nil
+                    request.url = components.url
+                }
+
+                return request
+            }
+        }
+        
+        let testDelegate = TestMockDuckDelegate()
+        MockDuck.delegate = testDelegate
+
+        let url1: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=bar#utm_term=ugh")
+        let url2: URL! = URL(string: "https://www.buzzfeed.com/so-many-tests?foo=baz#utm_term=ouch")
+        var request1 = URLRequest(url: url1)
+        var request2 = URLRequest(url: url2)
+        request1.httpBody = Data(bytes: [1, 2, 3, 4])
+        request2.httpBody = Data(bytes: [4, 3, 2, 1])
+        XCTAssertEqual(request1.requestHash, request2.requestHash)
+    }
+}
